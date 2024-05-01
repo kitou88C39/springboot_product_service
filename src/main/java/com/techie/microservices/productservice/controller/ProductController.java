48c63bf44bd78b23a166552ded91ecee0883b053
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-01 21:09:22 +0900

    controller/ProductController.javaを作成する

--- src/main/java/com/techie/microservices/productservice/controller/ProductController.java
+++ src/main/java/com/techie/microservices/productservice/controller/ProductController.java
@@ -1,5 +1,11 @@
 package com.techie.microservices.productservice.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api/product")
+
 public class ProductController {
 
 }
