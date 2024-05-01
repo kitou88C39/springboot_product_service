30e0a0cbe550e6234253c364ea569db0b7c699fc
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-01 21:04:51 +0900

    repository/ProductRepository.javaを作成する

--- src/main/java/com/techie/microservices/productservice/Repository/ProductRepository.java
+++ src/main/java/com/techie/microservices/productservice/Repository/ProductRepository.java
@@ -1,8 +1,8 @@
-package com.techie.microservices.productservice.Repository;
+package com.techie.microservices.productservice.repository;
 
 import com.techie.microservices.productservice.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ProductRepository extends MongoRepository<Product> {
+public interface ProductRepository extends MongoRepository<Product, String> {
 
 }
