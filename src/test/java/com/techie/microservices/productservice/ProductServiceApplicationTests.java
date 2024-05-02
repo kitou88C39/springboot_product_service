702b4ab384650760a07d95d94c8cff4ff32812fd
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-02 20:14:44 +0900

    test/java/com/teich/microservice/productservice/ProductServiceApplicationTests.javaを編集する

--- src/test/java/com/techie/microservices/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/techie/microservices/productservice/ProductServiceApplicationTests.java
@@ -2,12 +2,15 @@ package com.techie.microservices.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.MongoDBContainer;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo")
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
 	}
 
 }
