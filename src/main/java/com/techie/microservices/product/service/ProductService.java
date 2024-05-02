ff5c9bc11c0470c37b9b567c8f58f2583f50565d
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-02 14:09:21 +0900

    service/ProductService.javaを作成する

--- src/main/java/com/techie/microservices/product/service/ProductService.java
+++ src/main/java/com/techie/microservices/product/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.techie.microservices.product.service;
 
+import com.techie.microservices.product.dto.ProductRequest;
+import com.techie.microservices.product.model.Product;
 import com.techie.microservices.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,8 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+            .name(ProductRequest.name())
 
     }
 }
