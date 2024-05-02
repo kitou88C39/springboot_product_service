453b9615f19d7b0bb0f7d64714478625d7ebd201
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-02 14:56:45 +0900

    controller/ProductController.javaを作成する

--- src/main/java/com/techie/microservices/product/controller/ProductController.java
+++ src/main/java/com/techie/microservices/product/controller/ProductController.java
@@ -22,7 +22,6 @@ public class ProductController {
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Product> getAllProducts() {
-        return new String();
+        return productService.getAllProducts();
     }
-
 }
